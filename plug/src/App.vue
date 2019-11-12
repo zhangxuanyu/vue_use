e2cab8020d63dbea8a68acd19a19b304c12f463b
zhangjun1 <zhangjun@bd-apaas.com>
2019-11-12 14:37:27 +0800

    使用mock代理接口

--- plug/src/App.vue
+++ plug/src/App.vue
@@ -6,8 +6,38 @@
 </template>
 
 <script>
+var Mock = require('mockjs')
+const axios = require('axios');
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return{
+
+    }
+  },
+  created(){
+    var base_url = 'http://localhost:8081/'
+    console.log(process.env.NODE_ENV)
+    Mock.mock(base_url+'user', 'get', function(options) {
+      return Mock.mock({
+        'list': [{
+            'id|+1': 1
+        }]
+      })
+    })
+    
+    axios.get(base_url+'user')
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });  
+  },
+
 }
 </script>
 
